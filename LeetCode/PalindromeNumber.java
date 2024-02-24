e24d12564f43c2480d2024b15c18872d988129b3
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2024-02-24 18:53:08 +0530

    Palindrome Number code

---
+++ LeetCode/PalindromeNumber.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number:");
+		int x = sc.nextInt();
+		int num = x;
+	      int sum =0;
+	      while(num>0)
+	      {
+	          int n = num%10;
+	          sum = sum * 10 + n;
+	          num = num / 10;
+	      }
+	      if(sum==x)
+	      {
+	          System.out.println("Palindrome");
+	      }
+	      else
+	      {
+	    	  System.out.println("Not Palindrome");
+	      }
+
+	}
+
+}
