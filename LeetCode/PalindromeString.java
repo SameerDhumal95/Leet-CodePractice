cb3d2225381ede730ad50210829490b059f18fb9
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2024-02-24 18:52:03 +0530

    Palindrome code

---
+++ LeetCode/PalindromeString.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+import java.util.Scanner;
+
+public class PalindromeString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string");
+		String str = sc.next().toLowerCase();
+		String rstr = "";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			rstr = rstr + str.charAt(i);
+		}
+		if(rstr.equals(str))
+		{
+			System.out.println("Palindrome");
+		}
+		else
+		{
+			System.out.println("Not Palindrome");
+		}
+
+	}
+
+}
