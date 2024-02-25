9535c96cff663681e07e17f6bc599f6f07f59d28
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2024-02-25 10:34:55 +0530

    Prime Number Series Code

---
+++ LeetCode/PrimeNumberSeries.java
@@ -0,0 +1,32 @@
+package LeetCode;
+
+public class PrimeNumberSeries {
+
+	public static void main(String[] args) {
+		int s = 1;
+		int e = 20;
+		if(s==0||s==1)
+		{
+			s=2;
+		}
+		for(int i=s;i<=e;i++)
+		{
+			boolean flag = true;
+			for(int j=2;j<i;j++)
+			{
+				if(i%j==0)
+				{
+					flag=false;
+					break;
+				}
+			}
+			if(flag)
+			{
+				System.out.print(i+",");
+			}
+		}
+	}
+
+
+
+}
