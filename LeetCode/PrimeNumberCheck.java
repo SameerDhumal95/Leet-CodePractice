7bf3bd0d3bfe72d68ef945a2885f1b9d5aa503d0
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2024-02-25 10:23:58 +0530

    Prime Number Check Code

---
+++ LeetCode/PrimeNumberCheck.java
@@ -0,0 +1,37 @@
+package LeetCode;
+
+import java.util.Scanner;
+
+public class PrimeNumberCheck {
+
+	public static void main(String[] args) {
+		
+		int num = 23;
+		boolean flag = false;
+		if(num==0||num==1)
+		{
+			num=2;
+		}
+		else
+		{
+			for(int i=2;i<num;i++)
+			{
+				if(num%i==0)
+				{
+					flag=true;
+				}
+			}
+			if(flag)
+			{
+				System.out.println("Not Prime");
+			}
+			else
+			{
+				System.out.println("Prime");
+			}
+		}
+		
+
+	}
+
+}
