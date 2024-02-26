efb93ed3fb70f1ffdb8f0715402b769a1a58c7d8
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2024-02-26 08:49:17 +0530

    Sort an array using Selection sort algo

---
+++ LeetCode/SortArray.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+public class SortArray {
+
+	public static void main(String[] args) {
+		//SELECTION SORT
+		int[] arr = {2,5,3,8,1,9,15,73,4,12};
+		for(int i=0;i<arr.length;i++) 
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				int temp=0;
+				if(arr[i]>arr[j])//2>1
+				{
+				temp = arr[i];//2
+				arr[i]=arr[j];//1
+				arr[j]=temp;//1
+				}
+			}
+		}
+		for(int i=0;i<arr.length;i++)
+		System.out.println(arr[i]);
+	}
+
+}
