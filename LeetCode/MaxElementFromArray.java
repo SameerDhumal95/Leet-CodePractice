38aa8a6aeac74bb9ba1ff304374393b276f9cf45
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2024-02-26 08:54:04 +0530

    Max element from array

---
+++ LeetCode/MaxElementFromArray.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class MaxElementFromArray {
+
+	public static void main(String[] args) {
+		 int[] arr = {5,8,4,1,7,16,20,15,18};
+		 
+		 int max = arr[1];
+		 for(int i=0;i<arr.length;i++)
+		 {
+			 if(arr[i]>max)
+			 {
+				 max = arr[i];
+				
+			 }
+		 }
+		 System.out.println("Max element is: "+max);
+
+	}
+
+}
